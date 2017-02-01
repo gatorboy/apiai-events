8d929dddef4c250039d20592c7d4d12a737d5e96
Satish Menedi <satishmenedi@gmail.com>
2017-01-31 21:38:13 -0800

    query event time

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = processRequest(req)
+    res = processEventsRequest(req)
 
     res = json.dumps(res, indent=4)
     # print(res)
@@ -31,8 +31,9 @@ def webhook():
     return r
 
 
-def processRequest(req):
-    if req.get("result").get("action") != "event.location":
+def processEventsRequest(req):
+    action = req.get("result").get("action")
+    if !action.startswith("event"):
         return {}
     baseurl = "https://dfmobile-tzorg.cs12.force.com/customers/services/apexrest/events?"
     eventname = getEventName(req)
@@ -40,19 +41,19 @@ def processRequest(req):
         return {}
     url = baseurl + urllib.parse.urlencode({'names': eventname})
     
-    print("url:")
-    print(url)
-    
     result = urllib.request.urlopen(url).read()
     data = json.loads(result)
     
-    print("processRequest:")
-    print(json.dumps(data, indent=4))
-    
-    res = getEventVenue(data)
+    res = routeAndRespond(action, data)
     return res
 
-
+def routeAndRespond(action, data):
+    if action == event.location:
+        return getEventVenue(data)
+    elif action == event.time:
+        return getEventTime(data)
+    return {}    
+        
 def getEventName(req):
     result = req.get("result")
     parameters = result.get("parameters")
@@ -65,16 +66,11 @@ def getEventName(req):
 
 def getEventVenue(data):
     event = data[0]
-    print("Event:")
-    print(json.dumps(event, indent=4))
     if event is None:
         return {}
     
     name = event.get('Label__c')
-    
     venue = event.get('Venue__r')
-    print("Venue:")
-    print(json.dumps(venue, indent=4))
     if venue is None:
         return {}
     
@@ -85,7 +81,36 @@ def getEventVenue(data):
     if (city is None) or (country is None) or (state is None):
         return {}
     
-    speech = "The venue for the event " + name + "is " + city + ", " + state + ", " + country
+    speech = "The venue for the event " + name + " is " + city + ", " + state + ", " + country
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-events"
+    }
+
+def getEventTime(data):
+    event = data[0]
+    if event is None:
+        return {}
+        
+    name = event.get('Label__c')
+    starttime = event.get('Event_Start_Date__c')
+    endtime = event.get('Event_End_Date__c')
+
+    
+    if (starttime is None):
+        return {}
+    
+    speech = name + " starts from " + starttime
+    
+    if (endtime is not None):
+        speech = speech + " and ends at " + endtime
 
     print("Response:")
     print(speech)
