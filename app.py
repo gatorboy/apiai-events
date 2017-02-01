a3ce3775afd98c6b73f23588dd776f7ac88a1bf7
Satish Menedi <satishmenedi@gmail.com>
2017-01-31 17:20:00 -0800

    first commit-get event location

---
+++ app.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from future import standard_library
+standard_library.install_aliases()
+import urllib.request, urllib.parse, urllib.error
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    if req.get("result").get("action") != "event.location":
+        return {}
+    baseurl = "https://dfmobile-tzorg.cs12.force.com/customers/services/apexrest/events?"
+    eventname = getEventName(req)
+    if eventname is None:
+        return {}
+    url = baseurl + urllib.parse.urlencode({'names': eventname}) + "&format=json"
+    result = urllib.request.urlopen(url).read()
+    data = json.loads(result)
+    res = getEventVenue(data)
+    return res
+
+
+def getEventName(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    event = parameters.get("event-name")
+    if event is None:
+        return None
+
+    return event
+
+
+def getEventVenue(data):
+    event = data.get(0)
+    if event is None:
+        return {}
+    
+    name = event.get('Label__c')
+    
+    venue = event.get('Venue__r')
+    if venue is None:
+        return {}
+    
+    city = venue.get('City__c')
+    country = venue.get('Country__c')
+    state = venue.get('State__c')
+    
+    if (location is None) or (item is None) or (units is None):
+        return {}
+    
+    speech = "The venue for the event " + name + "is " + city + ", " + state + ", " + country
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-events-webhook"
+    }
+
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
